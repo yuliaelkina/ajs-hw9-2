ca20741ca27e2e4d4f2e7abc387fcd1cd6088e11
yulia elkina <todg@mail.ru>
2021-06-25 23:30:39 +0300

    class Team done

---
+++ src/js/team.js
@@ -0,0 +1,23 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(hero) {
+    if (this.members.has(hero)) {
+      throw new Error('Персонаж уже выбран');
+    }
+    this.members.add(hero);
+  }
+
+  addAll(...heroes) {
+    for (let hero of heroes) {
+      this.members.add(hero);
+    }
+  }
+
+  toArray() {
+    const heroesArray = Array.from(this.members);
+    return heroesArray;
+  }
+}
